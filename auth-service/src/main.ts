434cebd00b1ab7fcf6b397cb0b6e4a0bf560aaac
xedter20 <dextermiranda441@gmail.com>
2024-12-14 22:53:42 +0800

    fixes

--- auth-service/src/main.ts
+++ auth-service/src/main.ts
@@ -4,8 +4,10 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+
+  console.log('jhammsmmss');
   app.useLogger(app.get(Logger));
-  const port = 3001;
+  const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
   Logger.log(`Application is running onssss: ${await app.getUrl()}`);
 }
